6cfae6822fb69930b2984b7cc156dd99e9d05e8c
taiki-cano <dadadajyo@yahoo.co.jp>
2023-06-23 10:13:35 +0900

    tween animation

--- lib/screens/tween_animation_screen.dart
+++ lib/screens/tween_animation_screen.dart
@@ -10,14 +10,36 @@ class TweenAnimationScreen extends StatefulWidget {
 }
 
 class _TweenAnimationScreenState extends State<TweenAnimationScreen> {
+  double _value = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(TweenAnimationScreen.routeName),
-      ),
-      body: Center(),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text(TweenAnimationScreen.routeName),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TweenAnimationBuilder<double>(
+              duration: const Duration(milliseconds: 500),
+              tween: Tween(begin: 0.0, end: _value),
+              child: Container(width: 120, height: 120, color: Colors.red),
+              builder: (context, value, child) {
+                return Transform.translate(
+                  offset: Offset(value * 200 - 100, 0),
+                  child: child,
+                );
+              },
+            ),
+            Slider.adaptive(
+              value: _value,
+              onChanged: (value) => setState(() {
+                _value = value;
+              }),
+            ),
+          ],
+        ));
   }
 }
